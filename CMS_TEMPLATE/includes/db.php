f4ec96a16194406761d952cf8f8ccb2a40d76fb3
hijani <hijan29714@gmail.com>
2022-09-10 15:03:53 +0000

    CHANGES

--- CMS_TEMPLATE/includes/db.php
+++ CMS_TEMPLATE/includes/db.php
@@ -5,10 +5,10 @@
     $db['db_password'] = "";
     $db['db_name'] = "cms";
 
-    // foreach($db[$key] => $value) {
-
-    // }
-    $connection = mysqli_connect("localhost", "root", "", "cms");
+    foreach($db as $key => $value) {
+        define(strtoupper($key), $value);
+    }
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     if($connection) {
         echo "hell";
     }
