8c6b0da3a1521c9aeaa3ae1bcee85c65b5133628
hijani <hijan29714@gmail.com>
2022-09-10 11:17:04 +0000

    changes

--- CMS_TEMPLATE/includes/db.php
+++ CMS_TEMPLATE/includes/db.php
@@ -5,6 +5,10 @@
     $db['db_password'] = "";
     $db['db_name'] = "cms";
 
+    // foreach($db[$key] => $value) {
+
+    // }
+    $connection = mysqli_connect("localhost", "root", "", "cms");
 
 
 ?>
