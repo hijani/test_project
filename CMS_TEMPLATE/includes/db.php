aea33f33f1993b9d89b02cd67a2d9eae8e4fa922
hijani <hijan29714@gmail.com>
2022-09-10 11:10:56 +0000

    changes

--- CMS_TEMPLATE/includes/db.php
+++ CMS_TEMPLATE/includes/db.php
@@ -1,9 +1,9 @@
 <?php 
 
     $db['db_host'] = "localhost";
-    $db['db_host'] = "localhost";
-    $db['db_host'] = "localhost";
-    $db['db_host'] = "localhost";
+    $db['db_user'] = "localhost";
+    $db['db_password'] = "localhost";
+    $db['db_name'] = "localhost";
 
 
 
