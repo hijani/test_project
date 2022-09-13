e9e90940c8093db5092983dea5d6b4db9e1f6d8f
hijani <hijan29714@gmail.com>
2022-09-13 14:30:23 +0000

    changes

--- CMS_TEMPLATE/includes/add_post.php
+++ CMS_TEMPLATE/includes/add_post.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <title>New Post</title>
 </head>
 <body>
     <h1 class="text-center">New Post</h1>
-    <form action=""></form>
+    <form action="insert_post.php" method="post">
+        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
+        <input type="submit" name="submit" class="form-control">
+    </form>
 
 
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.js"></script>
 </body>
 </html>
