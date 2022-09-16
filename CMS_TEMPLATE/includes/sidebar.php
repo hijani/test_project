3c48f6a3cab73a0e062247fc855fddcfab23b0b0
hijani <hijan29714@gmail.com>
2022-09-16 11:03:26 +0000

    cjhends

--- CMS_TEMPLATE/includes/sidebar.php
+++ CMS_TEMPLATE/includes/sidebar.php
@@ -1,11 +1,20 @@
+<?php include "db.php"; ?>
 <div class="col-md-4">
     <?php 
         
         if(isset($_POST['submit'])) {
+            global $connection;
             $search = $_POST['search'];
 
             $query = "SELECT * FROM posts";
             $query .= "WHERE post_tags = '$$search$'";
+            $search_connection = mysqli_query($connection, $query);
+
+            if (!$search_connection) {
+                die("query error" . mysqli_error($connection));
+            } 
+
+            
         }
 
 
