2075fc2d4d6db2798341f4b99e4bd5c6c1e8fd3e
hijani <hijan29714@gmail.com>
2022-09-17 02:39:12 +0000

    changes

---
+++ CMS_TEMPLATE/admin/categories.php
@@ -0,0 +1,24 @@
+<?php include "includes/header.php" ?>
+
+<body>
+
+    <div id="wrapper">
+    
+        <!-- Navigation -->
+        <?php include "includes/navigation.php"; ?>
+
+        <?php include "includes/page_wrapper.php" ?>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
--- CMS_TEMPLATE/admin/includes/page_wrapper.php
+++ CMS_TEMPLATE/admin/includes/page_wrapper.php
@@ -9,6 +9,17 @@
                     Welcome to CMS Admin
                     <small>Author</small>
                 </h1>
+
+                <div class="col-xs-6">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <input type="text" name="cat_title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" value="Add Categories" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
         <!-- /.row -->
