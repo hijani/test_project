e7ef3aac1a281ca73f766aa02954508a2de5b376
hijani <hijan29714@gmail.com>
2022-09-17 11:50:00 +0000

    chanege

--- CMS_TEMPLATE/admin/categories.php
+++ CMS_TEMPLATE/admin/categories.php
@@ -47,41 +47,7 @@
                                 </div>
                             </form>
 
-                            <form action="categories.php" method="post">
-                                <div class="form-group">
-                                    <label for="cat_title">Update Category</label>
-                                    <?php 
-                                        if (isset($_GET['edit'])) {
-                                            $edit_id = $_GET['edit'];
-
-                                            global $connection;
-                                            $nav_query = "SELECT * FROM categories WHERE cat_id = {$edit_id}";
-                                            $select_categories = mysqli_query($connection, $nav_query);
-
-                                            while ($row = mysqli_fetch_assoc($select_categories)){
-                                                $cat_id = $row['cat_id'];
-                                                $cat_title = $row['cat_title'];
-                                    ?>
-                                        <input type="text" value=<?php if (isset($cat_title)) {echo $cat_title;} ?> name="cat_title" class="form-control" placeholder="Category Title">
-                                    <?php 
-                                            }
-                                        } 
-                                    ?>
-
-                                    <?php 
-                                        if(isset($_POST['update'])) {
-                                            $delete_cat_title = $_POST['$cat_title'];
-                                            $update_query = "UPDATE categories SET cat_title = '{$cat_title}' WHERE cat_id = {$cat_id}";
-                                            $run_query = mysqli_query($connection, $update_query);
-                                            header("Location: categories.php");
-                                        }
-                                    ?>
-                                    
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" name="update" value="Update Category" class="btn btn-primary">
-                                </div>
-                            </form>
+                            
 
 
                         </div>
