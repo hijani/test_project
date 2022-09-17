b138496d8108a69b5cf416bc1bc28b51214a0298
hijani <hijan29714@gmail.com>
2022-09-17 03:44:28 +0000

    changes

--- CMS_TEMPLATE/admin/includes/top_menu.php
+++ CMS_TEMPLATE/admin/includes/top_menu.php
@@ -1,5 +1,5 @@
 <ul class="nav navbar-right top-nav">
-    
+    <li><a href="../index.php">Home Site</a></li>
     <li class="dropdown">
         
         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
