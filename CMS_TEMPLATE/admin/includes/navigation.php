68c24e90236d9859a25eb47f7d8cf2745768893c
hijani <hijan29714@gmail.com>
2022-09-17 02:13:58 +0000

    changes

---
+++ CMS_TEMPLATE/admin/includes/navigation.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">CMS Admin</a>
+    </div>
+
+    <!-- Top Menu Items -->
+    <?php include "top_menu.php" ?>
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <?php include "sidebar_menu.php" ?>
+    <!-- /.navbar-collapse -->
+
+</nav>
--- CMS_TEMPLATE/admin/includes/sidebar_menu.php
+++ CMS_TEMPLATE/admin/includes/sidebar_menu.php
@@ -4,16 +4,15 @@
             <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
         <li>
-            <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
-        </li>
-        <li>
-            <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
-        </li>
-        <li>
-            <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
-        </li>
-        <li>
-            <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
+            <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="posts_dropdown" class="collapse">
+                <li>
+                    <a href="#">View Al Posts</a>
+                </li>
+                <li>
+                    <a href="#">Add Posts</a>
+                </li>
+            </ul>
         </li>
         <li>
             <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
