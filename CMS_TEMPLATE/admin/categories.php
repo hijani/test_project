66dd22ff53e2e5b5aa0f9f01c342e9b0bf626eab
hijani <hijan29714@gmail.com>
2022-09-19 14:17:21 +0000

    changes

--- CMS_TEMPLATE/admin/categories.php
+++ CMS_TEMPLATE/admin/categories.php
@@ -46,48 +46,36 @@
                                     <input type="submit" name="submit" value="Add Category" class="btn btn-primary">
                                 </div>
                             </form>
-                            
-                            
+
                             <form action="categories.php" method="post">
                                 <div class="form-group">
                                     <label for="cat_title">Update Category</label>
+
                                     <?php 
+
                                         if (isset($_GET['edit'])) {
-                                            $edit_id = $_GET['edit'];
+                                            
+                                            $query = "SELECT * FROM categories";
+                                            $update_categories = mysqli_query($connection, $query);
 
-                                            global $connection;
-                                            $nav_query = "SELECT * FROM categories WHERE cat_id = {$edit_id}";
-                                            $select_categories = mysqli_query($connection, $nav_query);
-
-                                            while ($row = mysqli_fetch_assoc($select_categories)){
+                                            while ($row = mysqli_fetch_assoc($update_categories)){
                                                 $cat_id = $row['cat_id'];
                                                 $cat_title = $row['cat_title'];
-                                    ?>
-                                        <input type="text" value=<?php if (isset($cat_title)) {echo $cat_title;} ?> name="cat_title" class="form-control" placeholder="Category Title">
-                                    <?php 
-                                            }
-                                        } 
-                                    ?>
-
-                                    <?php 
-                                        if(isset($_POST['update'])) {
-                                            $update_cat_title = $_POST['cat_title'];
-                                            global $connection;
-                                            $update_query = "UPDATE categories SET cat_title = '{$update_cat_title}' WHERE cat_id = {$cat_id}";
-                                            $update_connect_query = mysqli_query($connection, $update_query);
-                                            header("Location: categories.php");
-
-                                            if(!$update_connect_query) {
-                                                echo "query failed" . mysqli_error($connection);
                                             }
                                         }
-                                    ?>
+                                        
                                     
+                                    ?>
+
+
+                                    <input type="text" name="cat_title" class="form-control" placeholder="Category Title">
                                 </div>
                                 <div class="form-group">
-                                    <input type="submit" name="update" value="Update Category" class="btn btn-primary">
+                                    <input type="submit" name="submit" value="Update Category" class="btn btn-primary">
                                 </div>
                             </form>
+                            
+
 
 
                         </div>
