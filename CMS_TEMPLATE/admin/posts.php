d8acd9d6fa2f279e4025ae16af2ede1ed82c8b6e
hijani <hijan29714@gmail.com>
2022-09-19 15:29:26 +0000

    csagag

--- CMS_TEMPLATE/admin/posts.php
+++ CMS_TEMPLATE/admin/posts.php
@@ -15,51 +15,9 @@
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header"> CRUD Categories</h1>
+                        <h1 class="page-header"> Posts</h1>
 
-                        <div class="col-xs-6">
-                            <?php inset_category(); ?>
-
-                            <form action="categories.php" method="post">
-                                <div class="form-group">
-                                    <label for="cat_title">Add Category</label>
-                                    <input type="text" name="cat_title" class="form-control" placeholder="Category Title">
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" name="submit" value="Add Category" class="btn btn-primary">
-                                </div>
-                            </form>
-
-                            <?php 
-                            
-                                if(isset($_GET['edit'])) {
-                                    $cat_id = $_GET['edit'];
-
-                                    include "includes/update_categories.php";
-                                }    
-
-                            ?>
-                            
-
-
-
-                        </div>
-
-                        <div class="col-xs-6">
-                            <table class="table table-bordered table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Category Title</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php view_category(); ?>
-
-                                    <?php delete_category(); ?>
-                                </tbody>
-                            </table>
-                        </div>
+                        
 
                     </div>
                 </div>
