5c19e24d29a24c5d296d1d6106a4dbe75bcdc361
hijani <hijan29714@gmail.com>
2022-09-19 16:08:02 +0000

    dsfsdf

--- CMS_TEMPLATE/admin/posts.php
+++ CMS_TEMPLATE/admin/posts.php
@@ -17,53 +17,30 @@
                     <div class="col-lg-12">
                         <h1 class="page-header"> Posts</h1>
 
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Category</th>
-                                    <th>Status</th>
-                                    <th>image</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Date</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                
-                            <?php 
-                                $query = "SELECT * FROM posts";
-                                $select_posts = mysqli_query($connection, $query);
-                                while ($row = mysqli_fetch_assoc($select_posts)) {
-                                    $post_id = $row['post_id'];
-                                    $post_author = $row['post_author'];
-                                    $post_title = $row['post_title'];
-                                    $post_category = $row['post_category_id'];
-                                    $post_status = $row['post_status'];
-                                    $post_image = $row['post_image'];
-                                    $post_tags = $row['post_tags'];
-                                    $post_comments = $row['post_comment_count'];
-                                    $post_date = $row['post_date'];
+<?php 
+    if(isset($_GET['source'])) {
+        $source = $_GET['source'];
 
-                                    echo "<tr>";
-                                    echo "<td>$post_id</td>";
-                                    echo "<td>$post_author</td>";
-                                    echo "<td>$post_title</td>";
-                                    echo "<td>$post_category</td>";
-                                    echo "<td>$post_status</td>";
-                                    echo "<td>$post_image</td>";
-                                    echo "<td>$post_tags</td>";
-                                    echo "<td>$post_comments</td>";
-                                    echo "<td>$post_date</td>";
-                                    echo "<tr>";
+    } else {
+        switch($source) {
+            case '34':
+            echo "nice34";
+            break;
+            case '100':
+            echo "nice100";
+            break;
+            case "200":
+            echo "nice200";
+            break;
+            default:
+            include "includes/view_all_posts.php";
+            break;
+
+        }
+    }
 
 
-                                }
-                            ?>
-                            </tbody>
-                        </table>
+?>                        
                         
 
                     </div>
