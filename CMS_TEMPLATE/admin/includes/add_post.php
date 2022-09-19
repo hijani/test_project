f4516800b21cde4c14028138cc04f4eec02d1b2f
hijani <hijan29714@gmail.com>
2022-09-19 17:14:14 +0000

    sdfsad

--- CMS_TEMPLATE/admin/includes/add_post.php
+++ CMS_TEMPLATE/admin/includes/add_post.php
@@ -2,50 +2,50 @@
 
     <div class="form-group">
         <label for="post_title">Post Title</label>
-        <input type="text clas
+        <input type="text" class="
         form-control" name="post_title">
     </div>
 
     <div class="form-group">
         <label for="post_category_id">Post Category</label>
-        <input type="text clas
+        <input type="text" class="
         form-control" name="post_category_id">
     </div>
 
     <div class="form-group">
         <label for="post_author">Post Author</label>
-        <input type="text clas
+        <input type="text" class="
         form-control" name="post_author">
     </div>
 
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text clas
+        <input type="text" class="
         form-control" name="post_status">
     </div>
 
     <div class="form-group">
         <label for="post_image">Post Image</label>
-        <input type="text clas
+        <input type="text" class="
         form-control" name="post_image">
     </div>
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text clas
+        <input type="text" class="
         form-control" name="post_tags">
     </div>
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <input type="text clas
+        <input type="text" class="
         form-control" name="post_content">
     </div>
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text clas
-        form-control" name="post_tags">
+        <input type="text" class="
+        form-control"c name="post_tags">
     </div>
 
 
