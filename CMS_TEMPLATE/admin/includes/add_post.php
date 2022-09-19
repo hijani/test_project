c638d66446063b0be9d129762125de40f5cafa3e
hijani <hijan29714@gmail.com>
2022-09-19 23:07:48 +0000

    dsfsd

--- CMS_TEMPLATE/admin/includes/add_post.php
+++ CMS_TEMPLATE/admin/includes/add_post.php
@@ -16,8 +16,11 @@
 
         move_uploaded_file($post_image_temp, "../images/$post_image");
 
-        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
-        $query .= " VALUES($post_category_id, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
+        // $query = "INSERT INTO posts    (post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
+        // $query .= " VALUES ($post_category_id, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', {$post_comment_count}, '{$post_status}')";
+
+        $query = "INSERT INTO posts (post_title) VALUES ('$post_title')";
+
         $post_connection = mysqli_query($connection, $query);
 
 
