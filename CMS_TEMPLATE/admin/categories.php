4587e1b43d2bb7546af152415db85b4fc55a6008
hijani <hijan29714@gmail.com>
2022-09-19 14:24:34 +0000

    changes

--- CMS_TEMPLATE/admin/categories.php
+++ CMS_TEMPLATE/admin/categories.php
@@ -54,21 +54,23 @@
                                     <?php 
 
                                         if (isset($_GET['edit'])) {
-                                            
-                                            $query = "SELECT * FROM categories";
+                                            $cat_id = $_GET['edit'];
+                                            $query = "SELECT * FROM categories WHERE cat_id = $cat_id ";
                                             $update_categories = mysqli_query($connection, $query);
 
                                             while ($row = mysqli_fetch_assoc($update_categories)){
                                                 $cat_id = $row['cat_id'];
                                                 $cat_title = $row['cat_title'];
-                                            }
-                                        }
-                                        
-                                    
+
                                     ?>
 
+                                    <input value="<?php if(isset($cat_title)) { echo $cat_title;} ?>" type="text" name="cat_title" class="form-control" placeholder="Category Title">
 
-                                    <input type="text" name="cat_title" class="form-control" placeholder="Category Title">
+                                    <?php   }
+                                        } ?>
+
+
+                                    
                                 </div>
                                 <div class="form-group">
                                     <input type="submit" name="submit" value="Update Category" class="btn btn-primary">
