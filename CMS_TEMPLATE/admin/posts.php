95169bbe289c28e0f2d0159f2cd4cba5d4a0fa7f
hijani <hijan29714@gmail.com>
2022-09-19 15:47:09 +0000

    dfdasf

--- CMS_TEMPLATE/admin/posts.php
+++ CMS_TEMPLATE/admin/posts.php
@@ -32,17 +32,36 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>autht</td>
-                                    <td>tiuttu</td>
-                                    <td>catet</td>
-                                    <td>stat</td>
-                                    <td>imghm</td>
-                                    <td>tagg</td>
-                                    <td>commm</td>
-                                    <td>dteee</td>
-                                </tr>
+                                
+                            <?php 
+                                $query = "SELECT * FROM posts";
+                                $select_posts = mysqli_query($connection, $query);
+                                while ($row = mysqli_fetch_assoc($select_posts)) {
+                                    $post_id = $row['post_id'];
+                                    $post_author = $row['post_author'];
+                                    $post_title = $row['post_title'];
+                                    $post_category = $row['post_category_id'];
+                                    $post_status = $row['post_status'];
+                                    $post_image = $row['post_image'];
+                                    $post_tags = $row['post_tags'];
+                                    $post_comments = $row['post_comment_count'];
+                                    $post_date = $row['post_date'];
+
+                                    echo "<tr>";
+                                    echo "<td>$post_id</td>";
+                                    echo "<td>$post_author</td>";
+                                    echo "<td>$post_title</td>";
+                                    echo "<td>$post_category</td>";
+                                    echo "<td>$post_status</td>";
+                                    echo "<td>$post_image</td>";
+                                    echo "<td>$post_tags</td>";
+                                    echo "<td>$post_comments</td>";
+                                    echo "<td>$post_date</td>";
+                                    echo "<tr>";
+
+
+                                }
+                            ?>
                             </tbody>
                         </table>
                         
