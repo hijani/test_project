ab785d6b04cfb862bc1c344ac2e9a29a178051af
hijani <hijan29714@gmail.com>
2022-09-19 22:53:38 +0000

    sdfsdf

--- CMS_TEMPLATE/admin/includes/add_post.php
+++ CMS_TEMPLATE/admin/includes/add_post.php
@@ -17,7 +17,7 @@
         move_uploaded_file($post_image_temp, "../images/$post_image");
 
         $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status)";
-        $query .= "VALUES($post_category_id, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$pots_comment_count}', '{$post_status}')";
+        $query .= " VALUES($post_category_id, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$pots_comment_count}', '{$post_status}')";
         $post_connection = mysqli_query($connection, $query);
 
 
