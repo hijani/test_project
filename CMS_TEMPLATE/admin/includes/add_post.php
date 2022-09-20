b9a90bc24a434a377609fb02ba351ab88a6df68f
hijani <hijan29714@gmail.com>
2022-09-20 13:08:25 -0400

    trt

--- CMS_TEMPLATE/admin/includes/add_post.php
+++ CMS_TEMPLATE/admin/includes/add_post.php
@@ -17,7 +17,7 @@
         move_uploaded_file($post_image_temp, "../images/$post_image");
 
         $query = "INSERT INTO posts (post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
-        $query .= "VALUES ({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{../images/$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
+        $query .= "VALUES ({$post_category_id}, '{$post_title}', '{$post_author}', now(), '../images/{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
 
         // $query = "INSERT INTO try (try_title) VALUES ('$post_title')";
 
