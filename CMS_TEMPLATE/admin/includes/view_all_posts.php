d458088461bacfc35cb2f50da6d948fe81caf115
hijani <hijan29714@gmail.com>
2022-09-21 22:23:19 -0400

    dfdf

--- CMS_TEMPLATE/admin/includes/view_all_posts.php
+++ CMS_TEMPLATE/admin/includes/view_all_posts.php
@@ -40,7 +40,7 @@
             echo "<td>$post_comments</td>";
             echo "<td>$post_date</td>";
             echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
-            echo "<td><a href='posts.php?source=edit_post'>Edit</a></td>";
+            echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
             echo "<tr>";
 
 
