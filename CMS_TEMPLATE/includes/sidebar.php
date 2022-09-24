592e86e1d863c60d4477bb4b3acd2bd47ec9d0ea
hijani <hijan29714@gmail.com>
2022-09-24 00:22:15 -0400

    vbnv

--- CMS_TEMPLATE/includes/sidebar.php
+++ CMS_TEMPLATE/includes/sidebar.php
@@ -1,4 +1,4 @@
-
+<?php include "db.php"; ?>
 <div class="col-md-4">
     
 
@@ -20,34 +20,27 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
+                    <?php 
+                    
+                        $query = "SELECT * FROM categories";
+                        $select_categories_sidebar = mysqli_query($connection, $query);
+                    
+                    ?>
                     <h4>Blog Categories</h4>
                     <div class="row">
                         <div class="col-lg-6">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                                <?php
+                                    while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                                        $cat_title =  $row['cat_title'];
+                                        $cat_id = $row['cat_id'];
+
+                                        echo "<li><a href='category.php?category=$cat_id'>$cat_title</a></li>";
+                                }
+                                ?>
+                                
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
                     </div>
                     <!-- /.row -->
                 </div>
