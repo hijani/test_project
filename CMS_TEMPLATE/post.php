c4f986e8ff40bfea9d02efc786281505995612e0
hijani <hijan29714@gmail.com>
2022-09-25 14:17:15 -0400

    dsfdsf

--- CMS_TEMPLATE/post.php
+++ CMS_TEMPLATE/post.php
@@ -57,16 +57,17 @@
 
                 <?php 
                 
-                        if(isset($_POST['create_comment'])) {
-                            $comment_post_id = $_GET['p_id'];
-                            $comment_author = $_POST['comment_author'];
-                            $comment_content = $_POST['comment_content'];
-                            $comment_email = $_POST['comment_email'];
+                    if(isset($_POST['create_comment'])) {
+                        $comment_post_id = $_GET['p_id'];
+                        $comment_author = $_POST['comment_author'];
+                        $comment_content = $_POST['comment_content'];
+                        $comment_email = $_POST['comment_email'];
 
-                            $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
-                            $query .= "VALUES ($comment_post_id, '$comment_author', '$comment_email', '$comment_content', 'unapproved', now()) ";
-                            $create_comment_query = mysqli_query($connection, $query);
-                        }
+                        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                        $query .= "VALUES ($comment_post_id, '$comment_author', '$comment_email', '$comment_content', 'unapproved', now()) ";
+                        $create_comment_query = mysqli_query($connection, $query);
+
+                    }
                 
                 ?>
 
@@ -95,32 +96,32 @@
                 
                     
                     
-                    <?php 
-                        $query = "SELECT * FROM comments WHERE comment_post_id = {$comment_post_id} ";
-                        $query .= " AND comment_status = 'Approved' ";
-                        $query .= " ORDER BY comment_id DESC";
-                        $select_approve_comments = mysqli_query($connection, $query);
+                <?php 
+                    $approve_query = "SELECT * FROM comments WHERE comment_post_id = {$single_post_id} ";
+                    $approve_query .= " AND comment_status = 'Approved' ";
+                    $approve_query .= " ORDER BY comment_id DESC";
+                    $select_approve_comments = mysqli_query($connection, $approve_query);
 
-                        while($row = mysqli_fetch_assoc($select_approve_comments)) {
-                            $comment_author = $row['comment_author'];
-                            $comment_date = $row['comment_date'];
-                            $comment_content = $row['comment_content'];
-                    ?>
+                    while($row = mysqli_fetch_assoc($select_approve_comments)) {
+                        $comment_author = $row['comment_author'];
+                        $comment_date = $row['comment_date'];
+                        $comment_content = $row['comment_content'];
+                ?>
 
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">
-                                <?php echo $comment_author; ?>
-                                <small><?php echo $comment_date; ?></small>
-                            </h4>
-                            <?php echo $comment_content; ?>
-                        </div>
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">
+                            <?php echo $comment_author; ?>
+                            <small><?php echo $comment_date; ?></small>
+                        </h4>
+                        <?php echo $comment_content; ?>
                     </div>
+                </div>
 
-                    <?php } ?>
+                <?php } ?>
 
                 
 
